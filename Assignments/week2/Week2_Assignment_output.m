f9a9874054cdc220073e8176ac020aadad086222
Melodyleaf <101041586+Melodyleaf@users.noreply.github.com>
2022-06-06 16:23:24 +0800

    Add files via upload

---
+++ Assignments/week2/Week2_Assignment_output.m
@@ -0,0 +1,63 @@
+Running warmUpExercise ...
+5x5 Identity Matrix:
+ans =
+
+Diagonal Matrix
+
+   1   0   0   0   0
+   0   1   0   0   0
+   0   0   1   0   0
+   0   0   0   1   0
+   0   0   0   0   1
+
+Program paused. Press enter to continue.
+Plotting Data ...
+Program paused. Press enter to continue.
+
+Testing the cost function ...
+With theta = [0 ; 0]
+Cost computed = 32.072734
+Expected cost value (approx) 32.07
+
+With theta = [-1 ; 2]
+Cost computed = 54.242455
+Expected cost value (approx) 54.24
+Program paused. Press enter to continue.
+
+Running Gradient Descent ...
+Theta found by gradient descent:
+-3.630291
+1.166362
+Expected theta values (approx)
+ -3.6303
+  1.1664
+
+For population = 35,000, we predict a profit of 4519.767868
+For population = 70,000, we predict a profit of 45342.450129
+Program paused. Press enter to continue.
+Visualizing J(theta_0, theta_1) ...
+>> submit
+== Submitting solutions | Linear Regression with Multiple Variables...
+Use token from last successful submission (sxwyjlu@gmail.com)? (Y/n): n
+Login (email address): sxwyjlu@gmail.com
+Token: pNrFSOJJstnuYPcG
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100  2474  100  1392  100  1082   2355   1831 --:--:-- --:--:-- --:--:--  4200
+warning: findstr is obsolete; use strfind instead
+warning: strmatch is obsolete; use strncmp or strcmp instead
+==
+==                                   Part Name |     Score | Feedback
+==                                   --------- |     ----- | --------
+==                            Warm-up Exercise |  10 /  10 | Nice work!
+==           Computing Cost (for One Variable) |  40 /  40 | Nice work!
+==         Gradient Descent (for One Variable) |  50 /  50 | Nice work!
+==                       Feature Normalization |   0 /   0 | Your answer is incorrect.
+==     Computing Cost (for Multiple Variables) |   0 /   0 | Your answer is incorrect.
+==   Gradient Descent (for Multiple Variables) |   0 /   0 | Your answer is incorrect.
+==                            Normal Equations |   0 /   0 | Your answer is incorrect.
+==                                   --------------------------------
+==                                             | 100 / 100 |
+==
+>>
+㔐
